e0bebbcc6a493ff12a3588b946424a08200322db
Tobias Büschel <tobiasbueschel@users.noreply.github.com>
2015-11-25 14:00:43 +0000

    Create Test.java

---
+++ Tutorial-4/Test.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class Test {
+
+	public static void main (String[] args){		
+	    long total = 0;
+		
+		// reads in files and stores them in String array
+		try {
+			for(int i = 0; i < args.length; i++){
+				BufferedReader in = new BufferedReader(new FileReader(args[i]));				
+			    	while (in.readLine() != null){
+			    		total++;
+			    	}
+				in.close();
+			}
+		    System.out.println("Total lines: " + total);
+		} catch (Exception e) {
+			System.out.println("File(s) not found");
+		}
+	}
+}
+
