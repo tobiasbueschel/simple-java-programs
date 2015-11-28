9868b93189c6000d39d81cafe9824fd53f5611ef
Tobias Büschel <tobiasbueschel@users.noreply.github.com>
2015-11-28 21:34:09 +0000

    Create ForLoop.java

---
+++ Tutorial-0/ForLoop.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * <dl>
+ * <dt>Purpose:
+ * <dd>Simple example program
+ *
+ * <dt>Description:
+ * <dd>This program demonstrates use of for loop
+ * </dl>
+ *
+ * @author Tobias Büschel
+ * @version $Date: 2015/10/01 13:32:11 $
+ *
+ */
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		// prints "message" from 0-9
+		for (int counter = 0; counter < 10; counter++) {
+			System.out.println("message" + counter);
+		}
+		
+		// prints "message" from 9-0
+		for (int counter = 9; counter >= 0; counter--) {
+			System.out.println("message" + counter);
+		}
+	}
+}
