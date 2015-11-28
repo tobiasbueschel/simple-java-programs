6096ceeb80a377fc0c864609faa1d27d5568faeb
Tobias Büschel <tobiasbueschel@users.noreply.github.com>
2015-11-28 21:36:24 +0000

    Create CurrentDate.java

---
+++ Tutorial-0/CurrentDate.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+/**
+ * <dl>
+ * <dt>Purpose:
+ * <dd>Simple example program
+ *
+ * <dt>Description:
+ * <dd>This program prints out the message Hello World and the current date
+ * </dl>
+ *
+ *
+ */
+
+public class ExerciseTwo {
+
+	public static void main(String[] args) {
+
+		Date date = new Date();
+
+		System.out.println("Hello World \n");
+		System.out.println(date.toString());
+	}
+
+}
